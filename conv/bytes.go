eeac7413dac8b7bccaa7592566dc153ad3938a72
ruauka <ruauka123@yandex.ru>
2024-09-05 00:48:57 +0300

    Remove commets from conv && math

--- conv/bytes.go
+++ conv/bytes.go
@@ -7,11 +7,11 @@ func BytesToString(b []byte) string {
 	return unsafe.String(unsafe.SliceData(b), len(b))
 }
 
-//// BytesToFloat64BestEffort parses floating-point number s.
-////
-//// It is equivalent to strconv.ParseFloat(string(b), 64), but is faster.
-////
-//// 0 is returned if the number cannot be parsed.
-//func BytesToFloat64BestEffort(b []byte) float64 {
-//	return StringToFloat64BestEffort(BytesToString(b))
-//}
+// BytesToFloat64BestEffort parses floating-point number s.
+//
+// It is equivalent to strconv.ParseFloat(string(b), 64), but is faster.
+//
+// 0 is returned if the number cannot be parsed.
+func BytesToFloat64BestEffort(b []byte) float64 {
+	return StringToFloat64BestEffort(BytesToString(b))
+}
